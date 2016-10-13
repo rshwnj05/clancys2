528977f1e541c3d3953343437d90a9b99deecff7
Bedonie <phelanzobedoniecc@gmail.com>
2016-10-13 11:05:56 -0600

    removed content from headUtils

--- lib/headutils.php
+++ lib/headutils.php
@@ -1,13 +1,5 @@
-<meta charset="utf-8">
-<title>Clancy's</title>
-
 <link rel="stylesheet" href="lib/css/bootstrap.css">
 
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css"
 rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1"
 crossorigin="anonymous">
-
-<link href="https://fonts.googleapis.com/css?family=Amatic+SC|Pacifico|Quicksand|Roboto+Mono|Yatra+One"
-rel="stylesheet">
-
-<link rel="stylesheet" href="lib/css/style.css">
