33606c9e12312bcc84a0411c8ec1be767cb28ebf
rshwnj05 <rshwnj05@yahoo.com>
2016-10-13 11:03:08 -0600

    added pages.
    added bootstrap.css and headutils.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+     <?php require_once("lib/headutils.php");?>
+  </head>
+  <body>
+    <div class="jumbotron">
+      <h1>Welcome to Clancy's</h1>
+    </div>
+    <h1>Hello</h1>
+  </body>
+</html>
