8ca4959fff8e373bc41c3b7689b7efda1ae566db
rshwnj05 <rshwnj05@yahoo.com>
2016-10-14 08:50:19 -0600

    added img to index

--- index.php
+++ index.php
@@ -2,11 +2,28 @@
 <html>
   <head>
      <?php require_once("lib/headutils.php");?>
+
+     <link rel="stylesheet" href="lib/css/bootstrap.css">
+      <link rel="stylesheet" href="lib/css/index.css">
   </head>
   <body>
-    <div class="jumbotron">
-      <h1>Welcome to Clancy's</h1>
+    <?php require_once("lib/partials/navbar.php");?>
+    <div class="jumbotron" id="jumbotron1">
+      <h1>Welcome to Clancy's2</h1>
     </div>
-    <h1>Hello</h1>
+
+    <div class="col-md-6">
+      <h3>Address:</h3>
+        <h1>2701 E. 20th - Farmington, NM 87402</h1>
+    </div>
+
+    <div class="col-md-6">
+      <h3>Phone No.</h3>
+        <h1>505-325-8176</h1>
+    </div
+
+    </div>
+
+    </div>
   </body>
 </html>
