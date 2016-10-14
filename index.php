70ef01469b8c5be152b4747290c81a0f40a8928b
rshwnj05 <rshwnj05@yahoo.com>
2016-10-14 09:36:10 -0600

    added links to index.php n headutils.php

--- index.php
+++ index.php
@@ -5,22 +5,24 @@
 
      <link rel="stylesheet" href="lib/css/bootstrap.css">
       <link rel="stylesheet" href="lib/css/index.css">
+      <link rel="stylesheet" href="lib/css/menu.css">
   </head>
   <body>
     <?php require_once("lib/partials/navbar.php");?>
     <div class="jumbotron" id="jumbotron1">
-      <h1>Welcome to Clancy's2</h1>
+      <h1 id="welcome">Welcome to Clancy's2</h1>
     </div>
 
-    <div class="col-md-6">
+    <div class="col-md-6 container">
       <h3>Address:</h3>
         <h1>2701 E. 20th - Farmington, NM 87402</h1>
     </div>
 
-    <div class="col-md-6">
+    <div class="col-md-6 container">
       <h3>Phone No.</h3>
         <h1>505-325-8176</h1>
     </div
+
 
     </div>
 
---
+++ menu.php
@@ -0,0 +1,50 @@
+<html>
+  <head>
+     <?php require_once("lib/headutils.php");?>
+
+  </head>
+  <body>
+    <?php require_once("lib/partials/navBar.php");?>
+<h1>Menu</h1>
+<p>
+  Irish and Sushi Restaurant located in Farmington, New Mexico.
+</p>
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-4">
+      <h3>Sushi</h3>
+      <img src="lib/images/sushi1.jpeg" class="img-responsive center-block" alt="Picture of Sushi">
+    </div>
+
+    <div class="col-md-4">
+      <h3>Beer</h3>
+      <img src="lib/images/sushi1.jpeg" class="img-responsive center-block" alt="Picture of Sushi">
+    </div>
+
+    <div class="col-md-4">
+      <h3>Soups</h3>
+      <img src="lib/images/sushi1.jpeg" class="img-responsive center-block" alt="Picture of Sushi">
+    </div>
+  </div>
+
+  <div class="row">
+      <div class="col-md-4">
+        <h3>Pizza</h3>
+        <img src="lib/images/sushi1.jpeg" class="img-responsive center-block" alt="Picture of Sushi">
+      </div>
+
+      <div class="col-md-4">
+        <h3>Salad</h3>
+        <img src="lib/images/sushi1.jpeg" class="img-responsive center-block" alt="Picture of Sushi">
+      </div>
+
+      <div class="col-md-4">
+        <h3>More Sushi</h3>
+        <img src="lib/images/sushi1.jpeg" class="img-responsive center-block" alt="Picture of Sushi">
+      </div>
+    </div>
+  </div>
+
+  </body>
+</html>
