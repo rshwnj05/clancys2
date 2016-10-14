e31aa0823fca25349d2e6d17241917b0919f2829
Bedonie <phelanzobedoniecc@gmail.com>
2016-10-14 08:55:22 -0600

    about php page from yesterday

---
+++ about.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <?php require_once("lib/headutils.php");?>
+
+    <link rel="stylesheet" href="lib/css/bootstrap.css">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="lib/css/index.css">
+</head>
+
+<body>
+    <?php require_once("lib/partials/navbar.php");?>
+      <div class="jumbotron" id=jumbotron1">
+        <h1>Clancy's all about an Irish Experiment.</h1>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <h3>Sushi Menu</h3>
+                </div>
+                <div class="col-md-4">
+                    <h3>Burger Menu</h3>
+                </div>
+                <div class="col-md-4">
+                    <h3>Steak Menu</h3>
+                </div>
+
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img src="lib/images/sushiRolls.jpg" class="img-responsive center-block" style="height:200px; width:200px" alt="picture">
+                            <h3>Variety of Seafood</h3>
+                        </div>
+                        <div class="col-md-4">
+                            <img src="lib/images/hamburgerStack.jpg" class="img-responsive center-block" style="height:200px; width:200px" alt="picture">
+                            <h3>Build Your Own </h3>
+                        </div>
+                        <div class="col-md-4">
+                            <img src="lib/images/steak.jpg" class="img-responsive center-block" style="height:200px; width:200px" alt="picture">
+                            <h3>Tasty Steaks</h3>
+                        </div>
+                    </div </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
